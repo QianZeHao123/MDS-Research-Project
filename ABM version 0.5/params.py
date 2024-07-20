462481d1b1388b0b87b2d90d180b927f9718b9a3
Zehao Qian <zehao.qian.cn@gmail.com>
2024-07-20 15:26:24 +0100

    file to store parameters for the model

---
+++ ABM version 0.5/params.py
@@ -0,0 +1,10 @@
+# params.py
+
+params = {
+    "N": 1000,
+    "p": [0.01, 0.02, 0.03],
+    "q": 0.3,
+    "proportion_innovators": 0.01,
+    "proportion_influencers": 0.1,
+    "network_type": "small_world"
+}
