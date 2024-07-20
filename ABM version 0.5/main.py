810dfd231d2cf03ccce458e9780cfc9fc3a36536
Zehao Qian <zehao.qian.cn@gmail.com>
2024-07-20 15:26:04 +0100

    import parameters from a serperate file

--- ABM version 0.5/main.py
+++ ABM version 0.5/main.py
@@ -4,6 +4,8 @@ from model import BassModel
 import os
 import shutil
 
+# import parameters from params.py
+from params import params
 
 # --------------------------------------------
 # Create report directory
@@ -20,17 +22,6 @@ if os.path.exists(pic_dir):
     shutil.rmtree(pic_dir)
 os.makedirs(pic_dir)
 
-
-# --------------------------------------------
-# Parameters Setting
-params = {
-    "N": [500, 1000],
-    "p": [0.01, 0.02, 0.03],
-    "q": [0.3, 0.35, 0.4, 0.45, 0.5],
-    "proportion_innovators": [0.01, 0.02, 0.03],
-    "proportion_influencers": [0.05, 0.1],
-    "network_type": "small_world"
-}
 
 results = mesa.batch_run(
     BassModel,
