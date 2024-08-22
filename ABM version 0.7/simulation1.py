5f20d9d2f88253bfdc03d61b7c5a6a815fc35e69
Zehao Qian <zehao.qian.cn@gmail.com>
2024-08-22 13:29:01 +0100

    remove original scripts for simulations

--- ABM version 0.7/simulation1.py
+++ ABM version 0.7/simulation1.py
@@ -1,42 +1,11 @@
-# import pandas as pd
-# import mesa
-# from model import BassModel
-# import multiprocessing
-# from concurrent.futures import ProcessPoolExecutor
-# import os
-
-
-# """
-# Simulation 1: Change p and keep other parameters constant
-# - N = 1000
-# - q = 0.3
-# - Influencer proportion = 0.1
-# - Innovator proportion = 0.01
-# - network_type = "small_world"
-# """
-
-
-# if __name__ == '__main__':
-
-#     results_change_p = mesa.batch_run(
-#         BassModel,
-#         parameters={
-#             "N": 1000,
-#             "p": [0.01, 0.015, 0.02, 0.025, 0.03],
-#             "q": 0.3,
-#             "agent_proportion": [[0.001, 0.099, 0.009, 0.891]],
-#             "network_type": "small_world"
-#         },
-#         iterations=25,
-#         max_steps=100,
-#         number_processes=multiprocessing.cpu_count(),
-#         data_collection_period=1,
-#         display_progress=True,
-#     )
-
-#     results_df_results_change_p = pd.DataFrame(results_change_p)
-#     results_df_results_change_p.to_csv(
-#         './report/simulation1.csv', index=False)
+"""
+Simulation 1: Change p and keep other parameters constant
+- N = 1000
+- q = 0.3
+- Influencer proportion = 0.1
+- Innovator proportion = 0.01
+- network_type = "small_world"
+"""
 
 
 import pandas as pd
--- ABM version 0.7/simulation2.py
+++ ABM version 0.7/simulation2.py
@@ -1,37 +1,11 @@
-# import pandas as pd
-# import mesa
-# from model import BassModel
-# import multiprocessing
-
-# """
-# Simulation 2: Change q and keep other parameters constant
-# - N = 1000
-# - p = 0.02
-# - Influencer proportion = 0.1
-# - Innovator proportion = 0.01
-# - network_type = "small_world"
-# """
-
-# if __name__ == '__main__':
-#     results_change_q = mesa.batch_run(
-#         BassModel,
-#         parameters={
-#             "N": 1000,
-#             "p": 0.02,
-#             "q": [0.3, 0.35, 0.4, 0.45, 0.5],
-#             "agent_proportion": [[0.001, 0.099, 0.009, 0.891]],
-#             "network_type": "small_world"
-#         },
-#         iterations=25,
-#         max_steps=100,
-#         number_processes=multiprocessing.cpu_count(),
-#         data_collection_period=1,
-#         display_progress=True,
-#     )
-
-#     results_df_results_change_q = pd.DataFrame(results_change_q)
-#     results_df_results_change_q.to_csv(
-#         './report/simulation2.csv', index=False)
+"""
+Simulation 2: Change q and keep other parameters constant
+- N = 1000
+- p = 0.02
+- Influencer proportion = 0.1
+- Innovator proportion = 0.01
+- network_type = "small_world"
+"""
 
 
 import pandas as pd
